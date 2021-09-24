a750014762b5c0601e1374cf05c98f2d3f79c007
dzcsnvd <ext.ondrej.zelenka@skoda-auto.cz>
2021-09-24 14:05:54 +0300

    Initial commit

---
+++ src/main/kotlin/cz/softcake/module/pdf/ModulePdfApplication.kt
@@ -0,0 +1,11 @@
+package cz.softcake.module.pdf
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ModulePdfApplication
+
+fun main(args: Array<String>) {
+    runApplication<ModulePdfApplication>(*args)
+}
---
+++ src/test/kotlin/cz/softcake/module/pdf/ModulePdfApplicationTests.kt
@@ -0,0 +1,13 @@
+package cz.softcake.module.pdf
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ModulePdfApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
